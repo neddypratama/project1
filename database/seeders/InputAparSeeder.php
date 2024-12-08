89e2a3515e366e2449e36b69dae55585f4467c04
neddy pratama <neddypratama92@gmail.com>
2024-12-08 13:30:20 +0700

    New Seeder

--- database/seeders/InputAparSeeder.php
+++ database/seeders/InputAparSeeder.php
@@ -25,7 +25,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 2,
             'sub_uraian_id' => 2,
-            'hasil_apar' => 'Tabung telah terpakai',
+            'hasil_apar' => '1/0',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
@@ -34,7 +34,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 3,
             'sub_uraian_id' => 3,
-            'hasil_apar' => 'Baik',
+            'hasil_apar' => '0/1',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
@@ -43,7 +43,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 4,
             'sub_uraian_id' => 4,
-            'hasil_apar' => 'Menujukkan warna hijau',
+            'hasil_apar' => '0/1',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
@@ -52,7 +52,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 5,
             'sub_uraian_id' => 5,
-            'hasil_apar' => 'Tabung bersih',
+            'hasil_apar' => '0/1',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
@@ -61,7 +61,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 6,
             'sub_uraian_id' => 6,
-            'hasil_apar' => 'Baik',
+            'hasil_apar' => '1/0',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
@@ -70,7 +70,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 7,
             'sub_uraian_id' => 7,
-            'hasil_apar' => 'Baik',
+            'hasil_apar' => '1/0',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
@@ -79,7 +79,7 @@ class InputAparSeeder extends Seeder
         DB::table('input_apars')->insert([
             'input_apar_id' => 8,
             'sub_uraian_id' => 8,
-            'hasil_apar' => 'Tidak ada',
+            'hasil_apar' => '0/1',
             'revisi' => '',
             'apar_id' => '1',
             'created_at' => now(),
